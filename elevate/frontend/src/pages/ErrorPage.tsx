127cf381dae51a0a0cf3e01c54bbeff9a7617c1d
mikaelhachaturian <mikael.hach@hibob.io>
2023-12-28 08:52:09 +0200

    update error page

--- elevate/frontend/src/pages/ErrorPage.tsx
+++ elevate/frontend/src/pages/ErrorPage.tsx
@@ -1,19 +1,49 @@
-import { Box, Heading, Text } from '@chakra-ui/react';
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { Box, Button, Center, Heading, Text, VStack } from '@chakra-ui/react';
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom';
 
 const ErrorPage = () => {
   const error = useRouteError();
+  const navigate = useNavigate();
 
   return (
     <>
-      <Box padding={5}>
-        <Heading>Oops</Heading>
-        <Text>
-          {isRouteErrorResponse(error)
-            ? 'This page does not exist.'
-            : 'An unexpected error occurred.'}
-        </Text>
-      </Box>
+      <Center bg={'#DDD8D7'} height="100vh">
+        <Box
+          p={6}
+          borderWidth={1}
+          borderRadius={8}
+          boxShadow="lg"
+          bg={'#3E373D'}
+          h="200px"
+          w="350px"
+          maxH={'90%'}
+          maxW={'90%'}
+          color={'#DDD8C3'}
+          fontSize="xl"
+        >
+          <VStack spacing={3} justify={'space-between'}>
+            <Heading>Oops</Heading>
+            {isRouteErrorResponse(error) ? (
+              <>
+                <Text>This page does not exist. (404)</Text>
+                <Button
+                  bg={'#DDD8C3'}
+                  color={'#3E373D'}
+                  onClick={() => navigate('/')}
+                >
+                  Take Me Home
+                </Button>
+              </>
+            ) : (
+              'An unexpected error occurred.'
+            )}
+          </VStack>
+        </Box>
+      </Center>
     </>
   );
 };
