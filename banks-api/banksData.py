551d88d835ac1689d32ec8c00ddf4fdd281f5f27
AC2511 <aloncaspi3@gmail.com>
2024-01-06 16:34:03 +0200

    Added new offers

--- banks-api/banksData.py
+++ banks-api/banksData.py
@@ -1,53 +1,60 @@
 import random
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # List of offers
-banks = ['Leumi', 'Mizrahi', 'Poalim']
 Fixed = [
-    {'id': 1, 'sum': 250000, 'interest': 1.5, 'years': 30, 'monthlyFee': 980},
-    {'id': 2, 'sum': 250000, 'interest': 1.6, 'years': 25, 'monthlyFee': 1005},
-    {'id': 3, 'sum': 250000, 'interest': 1.7, 'years': 25, 'monthlyFee': 1075}
+    {"id": 1, "sum": 250000, "interest": 1.5, "years": 30, "monthlyFee": 980},
+    {"id": 2, "sum": 250000, "interest": 1.6, "years": 25, "monthlyFee": 1005},
+    {"id": 3, "sum": 250000, "interest": 1.7, "years": 25, "monthlyFee": 1075},
+    {"id": 4, "sum": 250000, "interest": 1.8, "years": 23, "monthlyFee": 1084},
+    {"id": 5, "sum": 250000, "interest": 1.9, "years": 21, "monthlyFee": 1099},
 ]
 
 Prime = [
-    {'id': 1, 'sum': 370000, 'interest': 2.1, 'years': 15, 'monthlyFee': 1621},
-    {'id': 2, 'sum': 370000, 'interest': 2.4, 'years': 10, 'monthlyFee': 1833},
-    {'id': 3, 'sum': 370000, 'interest': 2.7, 'years': 15, 'monthlyFee': 1743}
+    {"id": 1, "sum": 370000, "interest": 2.1, "years": 15, "monthlyFee": 1621},
+    {"id": 2, "sum": 370000, "interest": 2.4, "years": 10, "monthlyFee": 1833},
+    {"id": 3, "sum": 370000, "interest": 2.7, "years": 15, "monthlyFee": 1743},
+    {"id": 4, "sum": 370000, "interest": 2.8, "years": 8, "monthlyFee": 1999},
+    {"id": 5, "sum": 370000, "interest": 2.9, "years": 6, "monthlyFee": 1992},
 ]
 changesEvery5 = [
-    {'id': 1, 'sum': 380000, 'interest': 1.8, 'years': 15, 'monthlyFee': 1690},
-    {'id': 2, 'sum': 380000, 'interest': 1.9, 'years': 10, 'monthlyFee': 1856},
-    {'id': 3, 'sum': 380000, 'interest': 2, 'years': 15, 'monthlyFee': 1799}
+    {"id": 1, "sum": 380000, "interest": 1.8, "years": 15, "monthlyFee": 1690},
+    {"id": 2, "sum": 380000, "interest": 1.9, "years": 10, "monthlyFee": 1856},
+    {"id": 3, "sum": 380000, "interest": 2, "years": 15, "monthlyFee": 1799},
+    {"id": 4, "sum": 380000, "interest": 2.1, "years": 12 , "monthlyFee": 1772},
+    {"id": 5, "sum": 380000, "interest": 2.2, "years": 11, "monthlyFee": 1921},
 ]
 
 
 #
-@app.route('/random_bank', methods=['GET'])
+@app.route("/random_bank", methods=["GET"])
 def get_random_offer():
     # Randomize a bank
-    random_bank = random.randint(0, 2)
+    random_bank = random.randint(0, 4)
     # Randomize a fixed offer
-    random_id_fixed = random.randint(0, 2)
-    random_id_prime = random.randint(0, 2)
-    random_id_changes = random.randint(0, 2)
+    random_id_fixed = random.randint(0, 4)
+    random_id_prime = random.randint(0, 4)
+    random_id_changes = random.randint(0, 4)
 
     def get_years(random_id_fixed, random_id_prime, random_id_changes):
         totayears = 0
         for entry in Fixed:
-            if int(entry['id']) == int(random_id_fixed + 1):
-                totayears += int(entry['years'])
+            if int(entry["id"]) == int(random_id_fixed + 1):
+                totayears += int(entry["years"])
                 print(totayears)
                 break
         for entry1 in Prime:
-            if int(entry1['id']) == int(random_id_prime + 1):
-                totayears += int(entry1['years'])
+            if int(entry1["id"]) == int(random_id_prime + 1):
+                totayears += int(entry1["years"])
                 print(totayears)
                 break
         for entry2 in changesEvery5:
-            if int(entry2['id']) == int(random_id_changes + 1):
-                totayears += int(entry2['years'])
+            if int(entry2["id"]) == int(random_id_changes + 1):
+                totayears += int(entry2["years"])
                 print(totayears)
                 break
         return totayears
@@ -55,33 +62,33 @@ def get_random_offer():
     def get_monthly_payment(random_id_fixed, random_id_prime, random_id_changes):
         totalpay = 0
         for entry in Fixed:
-            if int(entry['id']) == int(random_id_fixed + 1):
-                totalpay += int(entry['monthlyFee'])
+            if int(entry["id"]) == int(random_id_fixed + 1):
+                totalpay += int(entry["monthlyFee"])
                 print(totalpay)
                 break
         for entry1 in Prime:
-            if int(entry1['id']) == int(random_id_prime + 1):
-                totalpay += int(entry1['monthlyFee'])
+            if int(entry1["id"]) == int(random_id_prime + 1):
+                totalpay += int(entry1["monthlyFee"])
                 print(totalpay)
                 break
         for entry2 in changesEvery5:
-            if int(entry2['id']) == int(random_id_changes + 1):
-                totalpay += int(entry2['monthlyFee'])
+            if int(entry2["id"]) == int(random_id_changes + 1):
+                totalpay += int(entry2["monthlyFee"])
                 print(totalpay)
                 break
         return totalpay
 
     response_data = {
-        'bank_name': banks[random_bank],
-        'Fixed offer': Fixed[random_id_fixed],
-        'Prime offer': Prime[random_id_prime],
-        'Changes every 5 years offer': changesEvery5[random_id_changes],
-        'Total years': get_years(random_id_fixed, random_id_prime, random_id_changes),
-        'Monthly payment:': get_monthly_payment(random_id_fixed, random_id_prime, random_id_changes)
+        "fixed": Fixed[random_id_fixed],
+        "prime": Prime[random_id_prime],
+        "changes_every_5_years_offer": changesEvery5[random_id_changes],
+        "monthly_payment:": get_monthly_payment(
+            random_id_fixed, random_id_prime, random_id_changes
+        ),
     }
-    return jsonify({'result': response_data})
+    return jsonify(response_data)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
 
