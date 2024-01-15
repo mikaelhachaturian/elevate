cb406790b2cc9d74f9da9d7e7d61e47757098546
mikaelhachaturian <mikael.hach@hibob.io>
2024-01-15 11:09:29 +0200

    added home page

--- elevate/frontend/src/pages/Home.tsx
+++ elevate/frontend/src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import { Heading } from '@chakra-ui/react';
+import { Image, Heading, VStack, Text } from '@chakra-ui/react';
+import logo from '../assets/elevate-icons/png/logo-no-background.png';
 
 export const Home = () => {
   return (
     <>
-      <Heading>Main</Heading>
+      <VStack spacing={4}>
+        <Image src={logo} boxSize="300px" objectFit="contain" />
+        <Heading>Apartment Purchasing Management System</Heading>
+        <Heading size={'md'}>Course Number: 27530301</Heading>
+        <Heading size={'md'}>Team: W80</Heading>
+        <Heading size={'md'}>Built by:</Heading>
+        <Text fontSize={'xl'}>Mikael Hachaturian</Text>
+        <Text fontSize={'xl'}>Alon Caspi</Text>
+        <Text fontSize={'xl'}>Ofek Lahiani</Text>
+      </VStack>
     </>
   );
 };
